9626051d3c2317ef010e1e03a13c342e9bd790d3
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2023-07-27 14:41:06 +0530

    ✨ Inheritance (classes)

--- DAY-1/Typescript/OOPS.js
+++ DAY-1/Typescript/OOPS.js
@@ -4,4 +4,45 @@
 //   city: string;
 // }
 // let company: ICompany = { name: "IBM", city: "Pune", xyz: 123 };
-var company = { name: "IBM", city: "Pune", xyz: 123 };
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+// let company: Company = { name: "IBM", city: "Pune", xyz: 123 };
+var company = { name: "IBM" };
+// class
+var Car = /** @class */ (function () {
+    function Car(name, speed) {
+        if (name === void 0) { name = "BMW"; }
+        if (speed === void 0) { speed = 200; }
+        this.name = name;
+        this.speed = speed;
+    }
+    Car.prototype.accelerate = function () {
+        console.log("The car " + this.name + " is running @ " + this.speed + " kmph !");
+    };
+    return Car;
+}());
+// let carObj = new Car();
+// carObj.accelerate();
+// Inheritance
+var JamesBondCar = /** @class */ (function (_super) {
+    __extends(JamesBondCar, _super);
+    function JamesBondCar(name, speed, isArmed) {
+        return _super.call(this, name, speed) || this;
+    }
+    return JamesBondCar;
+}(Car));
+var jbc = new JamesBondCar("Aston Martin", 300, true);
+jbc.accelerate();
--- DAY-1/Typescript/OOPS.ts
+++ DAY-1/Typescript/OOPS.ts
@@ -11,3 +11,36 @@ type Company = {
 };
 // let company: Company = { name: "IBM", city: "Pune", xyz: 123 };
 let company: Company = { name: "IBM" };
+
+// class
+class Car {
+  //private id: number;
+  public name: string;
+  public speed: number;
+
+  constructor(name: string = "BMW", speed: number = 200) {
+    this.name = name;
+    this.speed = speed;
+  }
+
+  accelerate(): void {
+    console.log(
+      "The car " + this.name + " is running @ " + this.speed + " kmph !",
+    );
+  }
+}
+
+// let carObj = new Car();
+// carObj.accelerate();
+
+// Inheritance
+class JamesBondCar extends Car {
+  isArmed: boolean;
+  constructor(name: string, speed: number, isArmed: boolean) {
+    super(name, speed);
+  }
+}
+
+let jbc = new JamesBondCar("Aston Martin", 300, true);
+
+jbc.accelerate();
