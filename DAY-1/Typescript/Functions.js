ce29e2443de972567b5be5e37055152a9d4f9f47
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2023-07-27 12:29:29 +0530

    ✨ Associating multiple types

--- DAY-1/Typescript/Functions.js
+++ DAY-1/Typescript/Functions.js
@@ -4,3 +4,16 @@ function printBook(author, title) {
     return "The book " + title + " is written by " + author;
 }
 console.log(printBook("Dr. APJ Abdul Kalam", "Wings of Fire"));
+var x;
+x = 100;
+x = "Hello";
+// Hands-on
+// Add(x,y) -> accepts two numbers & returns either string(error) if x is zero or number (addition)
+function Add(x, y) {
+    if (x === 0 || y === 0) {
+        return "x or y can not be zero";
+    }
+    return x + y;
+}
+var result = Add(4, 5);
+console.log(result);
--- DAY-1/Typescript/Functions.ts
+++ DAY-1/Typescript/Functions.ts
@@ -6,3 +6,19 @@ function printBook(author: string, title: string): string {
 }
 
 console.log(printBook("Dr. APJ Abdul Kalam", "Wings of Fire"));
+
+let x: string | number;
+x = 100;
+x = "Hello";
+// Hands-on
+// Add(x,y) -> accepts two numbers & returns either string(error) if x is zero or number (addition)
+
+function Add(x: number, y: number): number | string {
+  if (x === 0 || y === 0) {
+    return "x or y can not be zero";
+  }
+
+  return x + y;
+}
+let result: number | string = Add(4, 5);
+console.log(result);
