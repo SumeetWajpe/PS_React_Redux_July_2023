56fb77e5929442776e3758e3da0145576ecd2d12
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2023-07-27 13:07:08 +0530

    ✨ Using arrow functions as callback functions

--- DAY-1/Typescript/Functions.js
+++ DAY-1/Typescript/Functions.js
@@ -1,68 +1,52 @@
 // Argument types
-
+var _this = this;
 // function printBook(author: string, title: string): string {
 //   //   console.log(author, title);
 //   return "The book " + title + " is written by " + author;
 // }
-
 // console.log(printBook("Dr. APJ Abdul Kalam", "Wings of Fire"));
-
 // let x: string | number;
 // x = 100;
 // x = "Hello";
 // Hands-on
 // Add(x,y) -> accepts two numbers & returns either string(error) if x is zero or number (addition)
-
 // function Add(x: number, y: number): number | string {
 //   if (x === 0 || y === 0) {
 //     return "x or y can not be zero";
 //   }
-
 //   return x + y;
 // }
 // let result: number | string = Add(4, 5);
 // console.log(result);
-
 // Optional parameters
 // function Add(x?: number, y?: number) {
 //   console.log(x, y);
 // }
-
 // Add();
-
 // Default Parameters
 // function Add(x: number = 0, y: number = 0) {
 //   console.log(x, y);
 // }
-
 // Add();
 // Add(20, 10);
-
 // function printBook(author: string, ...titles: string[]) {
 //   console.log(author, titles);
 // }
-
 // printBook("Dr. APJ Abdul Kalam", "Ignited minds", "Wings Of Fire");
-
 // let Square = (x: number): number => x * x;
-
 // let cars: string[] = ["BMW", "AUDI", "MERC"];
-
 // cars.forEach((car: string) => console.log(car));
-
 // constructor
 function Emp() {
-  this.salary = 200000;
-  setTimeout(() => {
-    console.log(this.salary);
-  }, 3000);
+    var _this = this;
+    this.salary = 200000;
+    setTimeout(function () {
+        console.log(_this.salary);
+    }, 3000);
 }
-
-let e = new Emp();
-
-let EmpAsArrow = () => {
-  console.log(this);
-  this.salary = 500000;
+var e = new Emp();
+var EmpAsArrow = function () {
+    console.log(_this);
+    _this.salary = 500000;
 };
-
-let empArrow = new EmpAsArrow();
+var empArrow = new EmpAsArrow();
