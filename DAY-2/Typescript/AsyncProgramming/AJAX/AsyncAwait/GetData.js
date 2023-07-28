392e7e19b0da41c19d5f44521325fb60469badfb
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2023-07-28 13:55:26 +0530

    ✨ Using async/await

---
+++ DAY-2/Typescript/AsyncProgramming/AJAX/AsyncAwait/GetData.js
@@ -0,0 +1,14 @@
+function GetData() {
+  return new Promise((resolve, reject) => {
+    let xmlHttpreq = new XMLHttpRequest();
+    xmlHttpreq.open("GET", "https://jsonplaceholder.typicode.com/posts");
+    xmlHttpreq.onreadystatechange = function () {
+      if (xmlHttpreq.readyState == 4 && xmlHttpreq.status == 200) {
+        resolve(xmlHttpreq.responseText);
+      } else if (xmlHttpreq.readyState == 4 && xmlHttpreq.status !== 200) {
+        reject("Something went wrong - " + xmlHttpreq.status);
+      }
+    };
+    xmlHttpreq.send(); // places the async call
+  });
+}
