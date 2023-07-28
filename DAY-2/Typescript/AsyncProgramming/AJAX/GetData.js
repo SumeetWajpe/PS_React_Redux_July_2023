a7099d738fcbb4151f8430b042a01f0c659e2474
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2023-07-28 11:29:35 +0530

    ✨ Handling error - callback

--- DAY-2/Typescript/AsyncProgramming/AJAX/GetData.js
+++ DAY-2/Typescript/AsyncProgramming/AJAX/GetData.js
@@ -1,9 +1,11 @@
 function GetData(callback) {
   let xmlHttpreq = new XMLHttpRequest();
-  xmlHttpreq.open("GET", "https://jsonplaceholder.typicode.com/posts");
+  xmlHttpreq.open("GET", "https://jsonplaceholder.typicode.com/postsss");
   xmlHttpreq.onreadystatechange = function () {
     if (xmlHttpreq.readyState == 4 && xmlHttpreq.status == 200) {
-      callback(xmlHttpreq.responseText);
+      callback(null, xmlHttpreq.responseText);
+    } else if (xmlHttpreq.readyState == 4 && xmlHttpreq.status !== 200) {
+      callback("Something went wrong - " + xmlHttpreq.status, null);
     }
   };
   xmlHttpreq.send(); // places the async call
