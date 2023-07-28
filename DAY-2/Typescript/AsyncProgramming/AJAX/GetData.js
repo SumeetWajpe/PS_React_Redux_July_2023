9ff8e2411b81a22faadcb45762351a1feb778e74
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2023-07-28 11:24:21 +0530

    ✨ Using Callback methods

--- DAY-2/Typescript/AsyncProgramming/AJAX/GetData.js
+++ DAY-2/Typescript/AsyncProgramming/AJAX/GetData.js
@@ -1,9 +1,9 @@
-function GetData() {
+function GetData(callback) {
   let xmlHttpreq = new XMLHttpRequest();
   xmlHttpreq.open("GET", "https://jsonplaceholder.typicode.com/posts");
   xmlHttpreq.onreadystatechange = function () {
     if (xmlHttpreq.readyState == 4 && xmlHttpreq.status == 200) {
-      console.log(xmlHttpreq.responseText);
+      callback(xmlHttpreq.responseText);
     }
   };
   xmlHttpreq.send(); // places the async call
