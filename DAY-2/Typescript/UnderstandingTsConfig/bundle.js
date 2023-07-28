5f04b1faae92d549ca30b4a62d7d42091a59bbc7
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2023-07-28 09:05:06 +0530

    ✨ Ts config

---
+++ DAY-2/Typescript/UnderstandingTsConfig/bundle.js
@@ -0,0 +1,6 @@
+function Square(x) {
+    return x * x;
+}
+Square(10);
+let x = 100;
+const PI = 3.14;
---
+++ DAY-2/Typescript/UnderstandingTsConfig/index.ts
@@ -0,0 +1,8 @@
+function Square(x: number) {
+  return x * x;
+}
+
+Square(10);
+let x = 100;
+
+const PI = 3.14;
