8a2beb411ca6175d3c43da495605da0ffc6c6d23
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2023-07-28 12:13:32 +0530

    ✨ AJAX with AXIOS

--- DAY-2/Typescript/AsyncProgramming/AJAX/GetData.js
+++ DAY-2/Typescript/AsyncProgramming/AJAX/GetData.js
@@ -1,6 +1,6 @@
 function GetData(callback) {
   let xmlHttpreq = new XMLHttpRequest();
-  xmlHttpreq.open("GET", "https://jsonplaceholder.typicode.com/postsss");
+  xmlHttpreq.open("GET", "https://jsonplaceholder.typicode.com/posts");
   xmlHttpreq.onreadystatechange = function () {
     if (xmlHttpreq.readyState == 4 && xmlHttpreq.status == 200) {
       callback(null, xmlHttpreq.responseText);
