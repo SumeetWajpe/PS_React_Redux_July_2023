2338086d6410b7bbb4a325fe6f9158c84a6f335b
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2023-07-31 14:24:58 +0530

    ✨ Styling

--- DAY-3/my-first-app/src/components/app/App.tsx
+++ DAY-3/my-first-app/src/components/app/App.tsx
@@ -5,45 +5,44 @@ import { Message } from "../message/message.component";
 import { MessageModel } from "../../models/message.model";
 
 class App extends React.Component {
+  messages: MessageModel[] = [
+    {
+      message: "Hello",
+      from: "John",
+      to: "Kim",
+      imageUrl:
+        "https://img.freepik.com/free-vector/hello-wording-comic-speech-bubble-pop-art-style_1150-39959.jpg?w=2000",
+    },
+    {
+      message: "Hey",
+      from: "John",
+      to: "Kim",
+      imageUrl:
+        "https://i.scdn.co/image/ab6761610000e5eb7931715a93cf050c888145a3",
+    },
+    {
+      message: "Hola",
+      from: "John",
+      to: "Kim",
+      imageUrl:
+        "https://img.freepik.com/premium-vector/hola-hi-phrase-lettering-isolated-white-colourful-text-effect-design-vector-text-inscriptions-english-modern-creative-design-has-red-orange-yellow-colors_7280-7768.jpg?w=2000",
+    },
+    {
+      message: "By",
+      from: "John",
+      to: "Kim",
+      imageUrl:
+        "https://thumbs.dreamstime.com/b/neon-sign-good-bye-speech-bubble-frame-dark-background-neon-sign-good-bye-speech-bubble-frame-dark-background-light-151416182.jpg",
+    },
+  ];
   render(): React.ReactNode {
-    let messages: MessageModel[] = [
-      {
-        message: "Hello",
-        from: "John",
-        to: "Kim",
-        imageUrl:
-          "https://img.freepik.com/free-vector/hello-wording-comic-speech-bubble-pop-art-style_1150-39959.jpg?w=2000",
-      },
-      {
-        message: "Hey",
-        from: "John",
-        to: "Kim",
-        imageUrl:
-          "https://i.scdn.co/image/ab6761610000e5eb7931715a93cf050c888145a3",
-      },
-      {
-        message: "Hola",
-        from: "John",
-        to: "Kim",
-        imageUrl:
-          "https://img.freepik.com/premium-vector/hola-hi-phrase-lettering-isolated-white-colourful-text-effect-design-vector-text-inscriptions-english-modern-creative-design-has-red-orange-yellow-colors_7280-7768.jpg?w=2000",
-      },
-      {
-        message: "By",
-        from: "John",
-        to: "Kim",
-        imageUrl:
-          "https://thumbs.dreamstime.com/b/neon-sign-good-bye-speech-bubble-frame-dark-background-neon-sign-good-bye-speech-bubble-frame-dark-background-light-151416182.jpg",
-      },
-    ];
-
     return (
-      <div className="App">
+      <div>
         <header>
           <h1>All Messages</h1>
         </header>
-        <main>
-          {messages.map(msg => (
+        <main className="msg-container">
+          {this.messages.map(msg => (
             <Message messageDetails={msg} />
           ))}
         </main>
--- DAY-3/my-first-app/src/components/message/message.component.tsx
+++ DAY-3/my-first-app/src/components/message/message.component.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { MessageProps } from "../../props/message.props";
+import "./message.style.css";
 
 export class Message extends React.Component<MessageProps> {
   render(): React.ReactNode {
     // called for every Message component instance
     console.log("Within Message Render method !");
     return (
-      <>
+      <div className="msg-card">
         <img
           src={this.props.messageDetails.imageUrl}
           height="150px"
@@ -16,7 +17,7 @@ export class Message extends React.Component<MessageProps> {
         <h2>{this.props.messageDetails.message}</h2>
         <h3>From : {this.props.messageDetails.from}</h3>
         <h3>To : {this.props.messageDetails.to}</h3>
-      </>
+      </div>
     );
   }
 }
