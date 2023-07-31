f349719e821e97d70e967071b263dc8f7f1302de
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2023-07-31 14:09:19 +0530

    ✨ Dynamic generating Component instances

--- DAY-3/my-first-app/src/components/app/App.tsx
+++ DAY-3/my-first-app/src/components/app/App.tsx
@@ -6,27 +6,36 @@ import { MessageModel } from "../../models/message.model";
 
 class App extends React.Component {
   render(): React.ReactNode {
-    let messageOne: MessageModel = {
-      message: "Hello",
-      from: "John",
-      to: "Kim",
-      imageUrl:
-        "https://img.freepik.com/free-vector/hello-wording-comic-speech-bubble-pop-art-style_1150-39959.jpg?w=2000",
-    };
-    let messageTwo: MessageModel = {
-      message: "Hey",
-      from: "John",
-      to: "Kim",
-      imageUrl:
-        "https://i.scdn.co/image/ab6761610000e5eb7931715a93cf050c888145a3",
-    };
-    let messageThree: MessageModel = {
-      message: "Hola",
-      from: "John",
-      to: "Kim",
-      imageUrl:
-        "https://img.freepik.com/premium-vector/hola-hi-phrase-lettering-isolated-white-colourful-text-effect-design-vector-text-inscriptions-english-modern-creative-design-has-red-orange-yellow-colors_7280-7768.jpg?w=2000",
-    };
+    let messages: MessageModel[] = [
+      {
+        message: "Hello",
+        from: "John",
+        to: "Kim",
+        imageUrl:
+          "https://img.freepik.com/free-vector/hello-wording-comic-speech-bubble-pop-art-style_1150-39959.jpg?w=2000",
+      },
+      {
+        message: "Hey",
+        from: "John",
+        to: "Kim",
+        imageUrl:
+          "https://i.scdn.co/image/ab6761610000e5eb7931715a93cf050c888145a3",
+      },
+      {
+        message: "Hola",
+        from: "John",
+        to: "Kim",
+        imageUrl:
+          "https://img.freepik.com/premium-vector/hola-hi-phrase-lettering-isolated-white-colourful-text-effect-design-vector-text-inscriptions-english-modern-creative-design-has-red-orange-yellow-colors_7280-7768.jpg?w=2000",
+      },
+      {
+        message: "By",
+        from: "John",
+        to: "Kim",
+        imageUrl:
+          "https://thumbs.dreamstime.com/b/neon-sign-good-bye-speech-bubble-frame-dark-background-neon-sign-good-bye-speech-bubble-frame-dark-background-light-151416182.jpg",
+      },
+    ];
 
     return (
       <div className="App">
@@ -34,9 +43,9 @@ class App extends React.Component {
           <h1>All Messages</h1>
         </header>
         <main>
-          <Message messageDetails={messageOne} />
-          <Message messageDetails={messageTwo} />
-          <Message messageDetails={messageThree} />
+          {messages.map(msg => (
+            <Message messageDetails={msg} />
+          ))}
         </main>
       </div>
     );
