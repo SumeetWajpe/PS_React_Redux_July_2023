dd33f0e8a58bd26e13317000b63c61e7a7671939
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2023-07-31 16:11:17 +0530

    ✨ onlineshoppingapp

---
+++ DAY-3/onlineshoppingapp/src/components/app/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import ListOfProducts from "../listofproducts/listofproducts.component";
+
+class App extends React.Component {
+  render(): React.ReactNode {
+    return <ListOfProducts />;
+  }
+}
+
+export default App;
---
+++ DAY-3/onlineshoppingapp/src/components/product/product.component.tsx
@@ -0,0 +1,30 @@
+import React, { Component } from "react";
+import { ProductModel } from "../../models/product.model";
+
+type ProductProps = {
+  productdetails: ProductModel;
+};
+
+export default class Product extends Component<ProductProps> {
+  render() {
+    return (
+      <div className="col-md-3 my-1">
+        <div className="card">
+          <img
+            src={this.props.productdetails.imageUrl}
+            className="card-img-top"
+            alt={this.props.productdetails.title}
+            height="150px"
+            width="200px"
+          />
+          <div className="card-body">
+            <h5 className="card-title">{this.props.productdetails.title}</h5>
+            <p className="card-text">{this.props.productdetails.price}</p>
+            <p className="card-text">{this.props.productdetails.rating}</p>
+            <p className="card-text">{this.props.productdetails.likes}</p>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
