bb4a2718d69132eb9aafe19ca579c95bbc7d4d31
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2023-08-01 15:49:45 +0530

    ✨ Strong Typing Functional Component

--- DAY-3/my-first-app/src/components/functional/functional.component.tsx
+++ DAY-3/my-first-app/src/components/functional/functional.component.tsx
@@ -2,10 +2,18 @@
 //   return <h2>Hello !</h2>;
 // }
 
+import React from "react";
+
 // export let MessageFunctional = function () {
 //   return <h2>Hello !</h2>;
 // };
 
-export let MessageFunctional = (props: any) => {
+type MessageProps = {
+  message: string;
+};
+
+export let MessageFunctional: React.FC<MessageProps> = (
+  props: MessageProps,
+) => {
   return <h2>{props.message}</h2>;
 };
