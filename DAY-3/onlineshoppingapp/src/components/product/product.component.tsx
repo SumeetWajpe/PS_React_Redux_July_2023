c3645be99dc473a1bdc6505bc7ee7b3c425ce273
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2023-08-02 09:31:29 +0530

    ✨ ListOfProducts as functional component

--- DAY-3/onlineshoppingapp/src/components/product/product.component.tsx
+++ DAY-3/onlineshoppingapp/src/components/product/product.component.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { ProductModel } from "../../models/product.model";
 import Rating from "../rating/rating.component";
 
@@ -6,6 +7,7 @@ type ProductProps = {
 };
 
 let Product: React.FC<ProductProps> = (props: ProductProps) => {
+  let [currLikes, setCurrLikes] = useState(props.productdetails.likes);
   return (
     <div className="col-md-3 my-1">
       <div className="card">
@@ -27,10 +29,12 @@ let Product: React.FC<ProductProps> = (props: ProductProps) => {
           </p>
 
           <p className="card-text">₹.{props.productdetails.price}</p>
-          <button className="btn btn-outline-primary">
+          <button
+            className="btn btn-outline-primary"
+            onClick={() => setCurrLikes(currLikes + 1)}
+          >
             {/* {this.props.productdetails.likes}{" "} */}
-            {}
-            <i className="fa-regular fa-thumbs-up"></i>
+            {currLikes} <i className="fa-regular fa-thumbs-up"></i>
           </button>
           <button className="btn btn-outline-danger mx-1">
             <i className="fa-solid fa-trash"></i>
