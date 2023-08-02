58d7e7b3a3ad6303ada8bd038714570e6644fdf9
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2023-08-02 11:48:17 +0530

    ✨ Posts component

--- DAY-3/onlineshoppingapp/src/components/app/App.tsx
+++ DAY-3/onlineshoppingapp/src/components/app/App.tsx
@@ -2,10 +2,12 @@ import React from "react";
 
 import ListOfProducts from "../listofproducts/listofproducts.component";
 import GetProductById from "../getproductbyid/getproductbyid.component";
+import Posts from "../posts/posts.component";
 
 let App: React.FC = () => {
   // return <ListOfProducts />;
-  return <GetProductById />;
+  // return <GetProductById />;
+  return <Posts />;
 };
 
 export default App;
