d6f8038f64c73276bc27b8e8d0c0a83eb18e2860
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2023-08-02 10:23:43 +0530

    ✨ fetching the product based on the id

--- DAY-3/onlineshoppingapp/src/components/getproductbyid/getproductbyid.component.tsx
+++ DAY-3/onlineshoppingapp/src/components/getproductbyid/getproductbyid.component.tsx
@@ -1,11 +1,20 @@
+import { useEffect, useState } from "react";
+
 let GetProductById: React.FC = () => {
+  let [productId, setProductId] = useState(1);
+
+  useEffect(() => {
+    fetch("http://localhost:3500/products/" + productId)
+      .then(res => res.json())
+      .then(product => console.log(product));
+  });
   return (
     <>
       <label htmlFor="txtProductId">Product Id : </label>
       <input
         type="text"
         id="txtProductId"
-        onInput={(e: any) => console.log(e.target.value)}
+        onInput={(e: any) => setProductId(e.target.value)}
       />
     </>
   );
