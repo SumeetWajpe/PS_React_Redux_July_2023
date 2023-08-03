fec01a576718330770168c0c5824f880a90eb808
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2023-08-03 09:38:15 +0530

    ✨ using Link

--- DAY-3/onlineshoppingapp/src/components/app/App.tsx
+++ DAY-3/onlineshoppingapp/src/components/app/App.tsx
@@ -4,14 +4,24 @@ import ListOfProducts from "../listofproducts/listofproducts.component";
 import GetProductById from "../getproductbyid/getproductbyid.component";
 import Posts from "../posts/posts.component";
 
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 
 let App: React.FC = () => {
   return (
     <BrowserRouter>
+      {/* <a href="/">Products</a> | <a href="/posts">Posts</a> */}
+      <Link to="/">Products</Link> | <Link to="/posts"> Posts </Link>
       <Routes>
         <Route path="/" element={<ListOfProducts />} />
         <Route path="/posts" element={<Posts />} />
+        <Route
+          path="*"
+          element={
+            <>
+              <h1>Resource Not found !</h1>
+            </>
+          }
+        />
       </Routes>
     </BrowserRouter>
   );
