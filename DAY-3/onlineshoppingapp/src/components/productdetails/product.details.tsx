e0517fca84df69e0003cad5e227ce0c7a2e73840
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2023-08-03 11:21:36 +0530

    ✨ Getting Routing parameters in product details

--- DAY-3/onlineshoppingapp/src/components/productdetails/product.details.tsx
+++ DAY-3/onlineshoppingapp/src/components/productdetails/product.details.tsx
@@ -1,10 +1,12 @@
 import React from "react";
+import { useParams } from "react-router-dom";
 
 const ProductDetails: React.FC = () => {
+  const { id } = useParams();
   return (
     <div>
       <header>
-        <h1>Product Details</h1>
+        <h1>Product Details for {id}</h1>
       </header>
     </div>
   );
