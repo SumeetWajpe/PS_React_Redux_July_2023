6623701c1661c12e481b08e3fc2b08c3e6489b81
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2023-08-03 15:50:29 +0530

    ✨ configuring react-redux (provider & useSelector)

--- DAY-6/onlineshopping_redux/src/components/app/App.tsx
+++ DAY-6/onlineshopping_redux/src/components/app/App.tsx
@@ -1,4 +1,8 @@
+import { useSelector } from "react-redux";
+
 function App() {
+  const products = useSelector((store: any) => store.products);
+  console.log(products);
   return <div className="App">App</div>;
 }
 
