3aec6ca1c6d08d35d5500c14ceab7e546357395a
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2023-08-03 09:57:39 +0530

    ✨ Header (Nav)

--- DAY-3/onlineshoppingapp/src/components/app/App.tsx
+++ DAY-3/onlineshoppingapp/src/components/app/App.tsx
@@ -5,25 +5,30 @@ import GetProductById from "../getproductbyid/getproductbyid.component";
 import Posts from "../posts/posts.component";
 
 import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+import Header from "../header/header.component";
 
 let App: React.FC = () => {
   return (
-    <BrowserRouter>
-      {/* <a href="/">Products</a> | <a href="/posts">Posts</a> */}
-      <Link to="/">Products</Link> | <Link to="/posts"> Posts </Link>
-      <Routes>
-        <Route path="/" element={<ListOfProducts />} />
-        <Route path="/posts" element={<Posts />} />
-        <Route
-          path="*"
-          element={
-            <>
-              <h1>Resource Not found !</h1>
-            </>
-          }
-        />
-      </Routes>
-    </BrowserRouter>
+    <>
+      <BrowserRouter>
+        {/* <a href="/">Products</a> | <a href="/posts">Posts</a> */}
+        {/* <Link to="/">Products</Link> | <Link to="/posts"> Posts </Link> */}
+        <Header />
+        <Routes>
+          <Route path="/" element={<ListOfProducts />} />
+          <Route path="/posts" element={<Posts />} />
+          <Route path="/getproductbyid" element={<GetProductById />} />
+          <Route
+            path="*"
+            element={
+              <>
+                <h1>Resource Not found !</h1>
+              </>
+            }
+          />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 };
 
