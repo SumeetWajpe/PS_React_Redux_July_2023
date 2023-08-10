dd62a42fb602912193d14544d3b21ae5f1a6941c
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2023-08-10 10:23:37 +0530

    ✨ Changing Data (Context )

--- DAY-9/context-api/src/App.tsx
+++ DAY-9/context-api/src/App.tsx
@@ -2,13 +2,23 @@ import React, { useContext, useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
-const CounterContext = React.createContext<number>(0);
+type ContextDataType = {
+  count: number;
+  increment: () => void;
+};
+
+const CounterContext = React.createContext<ContextDataType>({
+  count: 0,
+  increment: function () {},
+});
 
 export function GrandParent() {
-  const [count] = useState(100);
+  const [count, setCount] = useState(100);
   return (
     <>
-      <CounterContext.Provider value={count}>
+      <CounterContext.Provider
+        value={{ count, increment: () => setCount(count + 1) }}
+      >
         <Parent />
       </CounterContext.Provider>
     </>
@@ -19,15 +29,30 @@ function Parent() {
   return (
     <>
       <Child />
+      <hr />
+      <AnotherChild />
     </>
   );
 }
 
 function Child() {
-  const ctx = useContext(CounterContext); // hook (consumer)
+  let ctx = useContext(CounterContext); // hook (consumer)
   return (
     <>
-      <p>Count : {ctx}</p>
+      <p>(Child)Count : {ctx.count}</p>
+      <button onClick={() => ctx.increment()}>++</button>
     </>
+  );
+}
+
+function AnotherChild() {
+  return (
+    <CounterContext.Consumer>
+      {ctx => (
+        <>
+          <p>(AnotherChild)Count : {ctx.count}</p>
+        </>
+      )}
+    </CounterContext.Consumer>
   );
 }
