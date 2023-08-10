ff837cc043e61034b778b434695cc3bbfd178b44
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2023-08-10 14:55:27 +0530

    ✨ Simulating click

--- DAY-9/unit-testing-app/src/counter/counter.tsx
+++ DAY-9/unit-testing-app/src/counter/counter.tsx
@@ -1,15 +1,34 @@
-import React, { Component } from "react";
+import React, { Component, useState } from "react";
 
-export default class Counter extends Component {
-  state = { count: 0 };
-  render() {
-    return (
+// export default class Counter extends Component<any, any> {
+//   constructor(props: any) {
+//     super(props);
+//     this.state = { count: this.props.initialCount };
+//   }
+
+//   render() {
+//     return (
+//       <div>
+//         <p>Count : {this.state.count}</p>
+//         <button onClick={() => this.setState({ count: this.state.count + 1 })}>
+//           ++
+//         </button>
+//       </div>
+//     );
+//   }
+// }
+
+const Counter = (props: any) => {
+  const [count, setCount] = useState(props.initialState);
+  return (
+    <div>
+      {" "}
       <div>
-        <p>Count : {this.state.count}</p>
-        <button onClick={() => this.setState({ count: this.state.count + 1 })}>
-          ++
-        </button>
+        <p>Count : {count}</p>{" "}
+        <button onClick={() => setCount(count + 1)}>++ </button>{" "}
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
+
+export default Counter;
--- DAY-9/unit-testing-app/src/test/counter.test.tsx
+++ DAY-9/unit-testing-app/src/test/counter.test.tsx
@@ -1,4 +1,4 @@
-import Enzyme, { shallow } from "enzyme";
+import Enzyme, { mount, shallow } from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
 import Counter from "../counter/counter";
 
@@ -9,7 +9,7 @@ describe("tests for Counter Component", () => {
     let ctrInstance = shallow(<Counter />);
     expect(ctrInstance).toBeDefined();
   });
-  it("tests for initial value of state to be zero", () => {
+  xit("tests for initial value of state to be zero", () => {
     // A A A -> Arrange -> Act -> Assert
 
     // Arrange
@@ -20,5 +20,38 @@ describe("tests for Counter Component", () => {
 
     // Assert
     expect(innerText).toEqual("Count : 0");
+  });
+  it("tests for initial value assigned to state", () => {
+    // A A A -> Arrange -> Act -> Assert
+
+    // Arrange
+    let ctrInstance = shallow(<Counter initialState={10} />);
+
+    // Act
+    let innerText = ctrInstance.find("p").text();
+
+    // Assert
+    expect(innerText).toEqual("Count : 10");
+  });
+  it("tests for state value to increment", () => {
+    // A A A -> Arrange -> Act -> Assert
+
+    // Arrange
+    let ctrInstance = shallow(<Counter initialState={10} />);
+
+    let button = ctrInstance.find("button");
+
+    let innerText = ctrInstance.find("p").text();
+
+    // Assert
+    expect(innerText).toEqual("Count : 10");
+
+    // Act
+    button.simulate("click"); // trigger the click event
+
+    innerText = ctrInstance.find("p").text();
+
+    // Assert
+    expect(innerText).toEqual("Count : 11");
   });
 });
