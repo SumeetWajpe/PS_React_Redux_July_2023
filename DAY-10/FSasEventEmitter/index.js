cf26a7c5ca6a3e1c7fc3c6146edf389f0c7c7c48
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2023-08-11 14:16:57 +0530

    ✨ Basic Http Server

--- DAY-10/FSasEventEmitter/index.js
+++ DAY-10/FSasEventEmitter/index.js
@@ -4,13 +4,15 @@ const readableStream = fs.createReadStream("input.txt", { encoding: "utf-8" });
 const writableStream = fs.createWriteStream("output.txt", {
   encoding: "utf-8",
 });
-let dataToBeWritten = "";
-readableStream.on("data", chunk => {
-  dataToBeWritten += chunk;
-  console.log(`>>>>>>>>>>>>>>>>> CHUNK >>>>>>>>>>>`);
-});
+// let dataToBeWritten = "";
+// readableStream.on("data", chunk => {
+//   dataToBeWritten += chunk;
+//   console.log(`>>>>>>>>>>>>>>>>> CHUNK >>>>>>>>>>>`);
+// });
 
-readableStream.on("end", () => {
-  writableStream.write(dataToBeWritten);
-  writableStream.end();
-});
+// readableStream.on("end", () => {
+//   writableStream.write(dataToBeWritten);
+//   writableStream.end();
+// });
+
+readableStream.pipe(writableStream);
