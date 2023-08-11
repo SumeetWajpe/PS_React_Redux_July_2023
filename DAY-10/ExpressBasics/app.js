ce1757b6d10a609e4fd98dc8dbf9d0b6d5df958f
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2023-08-11 15:12:21 +0530

    ✨ Serving Index.html

--- DAY-10/ExpressBasics/app.js
+++ DAY-10/ExpressBasics/app.js
@@ -3,7 +3,18 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  //   res.send("Hello World!");
+  console.log(__dirname);
+  res.sendFile("Index.html", { root: __dirname });
+});
+
+app.get("/products", (req, res) => {
+  let products = [
+    { id: 1, title: "Macbook" },
+    { id: 2, title: "Shoes" },
+  ];
+
+  res.json(products);
 });
 
 app.listen(port, () => {
