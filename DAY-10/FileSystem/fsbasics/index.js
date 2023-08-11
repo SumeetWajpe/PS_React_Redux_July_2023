639a449c46c7e01d4be93d652dbbcc5f05418eda
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2023-08-11 11:39:42 +0530

    ✨ Blocking vs Non-blocking

--- DAY-10/FileSystem/fsbasics/index.js
+++ DAY-10/FileSystem/fsbasics/index.js
@@ -1,12 +1,15 @@
 import fs from "fs";
 
 // non-blocking
-fs.readFile("input.txt", (err, dataFromTheFile) => {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log(dataFromTheFile.toString());
-  }
-});
+// fs.readFile("input.txt", (err, dataFromTheFile) => {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log(dataFromTheFile.toString());
+//   }
+// });
 
+//blocking
+let dataFromTheFile = fs.readFileSync("input.txt", { encoding: "utf-8" });
+console.log(dataFromTheFile);
 console.log("Program Ended !");
