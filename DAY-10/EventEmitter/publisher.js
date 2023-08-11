bc459e401617e8c80c02c307a93bc3b30907d8e1
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2023-08-11 12:23:21 +0530

    ✨ Publishing events & subscribing

--- DAY-10/EventEmitter/publisher.js
+++ DAY-10/EventEmitter/publisher.js
@@ -1,5 +1,21 @@
 import EventEmitter from "events";
 export function GetCount(maxIteration) {
   let e = new EventEmitter();
+
+  let counter = 1;
+  process.nextTick(() => {
+    e.emit("start");
+    let timer = setInterval(() => {
+      if (counter == 8) {
+        clearInterval(timer);
+        e.emit("error", counter);
+      }
+      if (counter == maxIteration) {
+        clearInterval(timer);
+        e.emit("end", counter);
+      }
+      e.emit("count", counter++);
+    }, 200);
+  });
   return e;
 }
--- DAY-10/EventEmitter/subscriber.js
+++ DAY-10/EventEmitter/subscriber.js
@@ -4,3 +4,12 @@ const evt = GetCount(10);
 evt.on("count", currCount => {
   console.log("Received count : " + currCount);
 });
+evt.on("start", () => {
+  console.log("Iteration started..");
+});
+evt.on("end", currCount => {
+  console.log("Iteration Ended with count " + currCount);
+});
+evt.on("error", currCount => {
+  console.log("Iteration Ended with error count " + currCount);
+});
