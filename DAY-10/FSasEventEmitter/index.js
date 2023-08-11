f284b993fe2aabc68f9e1c379ecba0f843252467
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2023-08-11 12:47:23 +0530

    ✨ writanle stream

--- DAY-10/FSasEventEmitter/index.js
+++ DAY-10/FSasEventEmitter/index.js
@@ -1,6 +1,16 @@
 import fs from "fs";
 
 const readableStream = fs.createReadStream("input.txt", { encoding: "utf-8" });
+const writableStream = fs.createWriteStream("output.txt", {
+  encoding: "utf-8",
+});
+let dataToBeWritten = "";
 readableStream.on("data", chunk => {
+  dataToBeWritten += chunk;
   console.log(`>>>>>>>>>>>>>>>>> CHUNK >>>>>>>>>>>`);
+});
+
+readableStream.on("end", () => {
+  writableStream.write(dataToBeWritten);
+  writableStream.end();
 });
