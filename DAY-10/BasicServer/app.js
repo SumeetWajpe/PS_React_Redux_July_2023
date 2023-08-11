9ae8abceff7fe2d9dd71276bb917932c98ba3518
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2023-08-11 14:28:46 +0530

    ✨ Serving file using fs in createServer

--- DAY-10/BasicServer/app.js
+++ DAY-10/BasicServer/app.js
@@ -1,12 +1,20 @@
 const http = require("http");
-
+const fs = require("fs");
 const hostname = "127.0.0.1";
 const port = 3000;
 
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader("Content-Type", "text/html");
-  res.end();
+  fs.readFile("Index.html", (err, data) => {
+    if (err) {
+      res.statusCode = 500;
+      res.setHeader("Content-Type", "text/html");
+      res.end("Something went wrong !");
+    } else {
+      res.statusCode = 200;
+      res.setHeader("Content-Type", "text/html");
+      res.end(data);
+    }
+  });
 });
 
 server.listen(port, hostname, () => {
