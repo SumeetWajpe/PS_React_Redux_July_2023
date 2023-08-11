63ce3ebf6ccdf279d91dc7af136b129f6995e455
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2023-08-11 14:41:53 +0530

    ✨ Serving css by checking req.url

--- DAY-10/BasicServer/app.js
+++ DAY-10/BasicServer/app.js
@@ -4,17 +4,34 @@ const hostname = "127.0.0.1";
 const port = 3000;
 
 const server = http.createServer((req, res) => {
-  fs.readFile("Index.html", (err, data) => {
-    if (err) {
-      res.statusCode = 500;
-      res.setHeader("Content-Type", "text/html");
-      res.end("Something went wrong !");
-    } else {
-      res.statusCode = 200;
-      res.setHeader("Content-Type", "text/html");
-      res.end(data);
-    }
-  });
+  if (req.url == "/") {
+    fs.readFile("Index.html", (err, data) => {
+      if (err) {
+        res.statusCode = 500;
+        res.setHeader("Content-Type", "text/html");
+        res.end("Something went wrong !");
+      } else {
+        res.statusCode = 200;
+        res.setHeader("Content-Type", "text/html");
+        res.end(data);
+      }
+    });
+  } else if (req.url == "/styles.css") {
+    fs.readFile("styles.css", (err, data) => {
+      if (err) {
+        res.statusCode = 500;
+        res.setHeader("Content-Type", "text/css");
+        res.end("Something went wrong !");
+      } else {
+        res.statusCode = 200;
+        res.setHeader("Content-Type", "text/css");
+        res.end(data);
+      }
+    });
+  } else {
+    res.statusCode = 404;
+    res.end("Resource Not Found !");
+  }
 });
 
 server.listen(port, hostname, () => {
