b9e436616d910ea5bc71d8eb69b3084b3e1a1e36
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2023-08-17 12:55:44 +0530

    ✨ Using Pug View Engine

--- DAY-11/usingpug/routes/users.js
+++ DAY-11/usingpug/routes/users.js
@@ -1,9 +1,8 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+const express = require("express");
+const router = express.Router();
+const users = [{ name: "jim" }, { name: "kim" }]; // database
+router.get("/", (req, res) => {
+  res.render("users", { users });
 });
 
 module.exports = router;
